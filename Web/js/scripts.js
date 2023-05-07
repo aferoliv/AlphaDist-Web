09bc58e26e702ff221cea1c25461ffde72973306
Sergio <3593635+Sergio-@users.noreply.github.com>
2023-05-07 14:35:01 -0300

    - Inclusão de Rotinas JS para dinamizar o carregamento/atualização dos elementos da página. - Re-estruturação do Codigo HTML para incluir IDs necessário.

--- Web/js/scripts.js
+++ Web/js/scripts.js
@@ -1,17 +1,62 @@
 // Scripts Especificos para Funcionalidade da Aplicação
-// 
 
+
+// Global para Guardar dados dos Sistemas
+array_Sistemas = [];
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+//Obtem Lista de Sistemas dinamicamente
+function getList_Sitemas() {
+    //Aqui deve ser adicionado o codigo para obter a lista de sistemas dinamicamente
+    return [
+        { id: 'Sis_A', nome: 'Sistema A', descricao: 'Esta é a Descrição do Sistema A.' },
+        { id: 'Sis_B', nome: 'Sistema B', descricao: 'Esta é a Descrição do Sistema B.' },
+        { id: 'Sis_C', nome: 'Sistema C', descricao: 'Esta é a Descrição do Sistema C.' },
+        { id: 'Sis_D', nome: 'Sistema D', descricao: 'Esta é a Descrição do Sistema D.' },
+        { id: 'Sis_E', nome: 'Sistema E', descricao: 'Esta é a Descrição do Sistema E.' },
+        { id: 'Sis_F', nome: 'Sistema F', descricao: 'Esta é a Descrição do Sistema F.' },
+    ];
+}
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// Esta função atualiza/preenche o Elemento dropdown_Sistemas com os valores existentes em array_Sistemas
+// Esta funcao também adiciona um Evento para chamar a funcão changeDropdown_Sistemas()
+// toda vez que um elemento for clicado.
+function updateDropdown_Sistemas() {
+    const dropdownMenu = document.getElementById('dropdown_SistemasMenu');
+    const dropdownButton = document.getElementById('dropdown_Sistemas');
+    dropdownMenu.innerHTML = ''; //Limpa Lista de Opcoes Atuais
+
+    //Este Loop Itera sobre cada elemento na variavel array_Sistemas
+    array_Sistemas.forEach(({ id, nome }) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('dropdown-item');
+        listItem.id = id;
+        listItem.textContent = nome;        
+        listItem.addEventListener('click', changeDropdown_Sistemas);        
+        dropdownMenu.appendChild(listItem);
+    });
+}
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// Esta função chama getList_Sitemas para obter a lista the Sistemas (id, nome, descricao)
+function changeDropdown_Sistemas(event) {
+    const selectedSistema = array_Sistemas.find((element) => element.id === event.target.id);
+    console.log("Sistema Selecionado: ", selectedSistema);    
+    document.getElementById('dropdown_Sistemas').textContent = selectedSistema.nome;
+    document.getElementById('text_SistemaDescricao').textContent = selectedSistema.descricao;
+    //Inclua aqui outros elementos/parametros que precisam ser alterados quando um novo
+    //sistema for selecionado.
+};
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+//Executa ações assim que a página for completamente carregada.
 window.addEventListener('DOMContentLoaded', event => {
     console.log("Pagina Carregada!");
-    }
-);
-
-
-//Modifica comportamento do Botão do Bootstrap para Exibir o Sistema selecionado após desfocar.
-document.querySelectorAll('.dropdown-menu a').forEach(item => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-        const dropdownButton = document.getElementById('dropdownMenuButton');
-        dropdownButton.innerHTML = e.target.innerHTML;
-    });
+    array_Sistemas = getList_Sitemas();
+    updateDropdown_Sistemas();
 });
